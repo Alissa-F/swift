4fe4c5712711f5f59f9fa4cdb772669279b0fc9f
Alexander Farber <Alexander.Farber@gmail.com>
2018-07-02 20:27:58 +0200

    Added ChecklistItem

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Alexander Farber on 02.07.18.
+//  Copyright © 2018 Alexander Farber. All rights reserved.
+//
+
+import Foundation
+class ChecklistItem {
+    var text = ""
+    var checked = false 
+}
